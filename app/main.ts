56dd67a28476ab7bfe065b73b7624763fffa7250
colin <colin.williams.dev@gmail.com>
2026-02-19 03:37:31 +0700

    :wrench: fix main extra logs and order of operations bugs

--- app/main.ts
+++ app/main.ts
@@ -6,14 +6,12 @@ import { dispatcher } from "./api";
 
 async function main() {
   try {
-
     const [, , flag, prompt] = process.argv;
     const API_KEY = process.env.OPENROUTER_API_KEY;
     const BASE_URL = process.env.OPENROUTER_BASE_URL ?? EXTERNAL_URLS.OPEN_ROUTER_BASE;
 
     /////////////////////////////////////
     // VALIDATE AUTHENTICATION & INPUT //
-    /////////////////////////////////////
     if (!API_KEY)
       throw new Error(ERRORS.MISSING_API_KEY);
     if (flag !== "-p" || !prompt)
@@ -21,7 +19,6 @@ async function main() {
 
     /////////////////////////////////////
     // INSTANTIATE AND REQUEST //////////
-    /////////////////////////////////////
     const client = new OpenAI({ apiKey: API_KEY, baseURL: BASE_URL });
 
     const response = await client.chat.completions.create({
@@ -32,16 +29,15 @@ async function main() {
 
     /* use print statements for debugging (visible when running tests) */
     console.error("Logs from your program will appear here!");
-    console.log(response.choices[0].message.content);
 
     /////////////////////////////////////
     // RESPONSE HANDLING ////////////////
-    /////////////////////////////////////
     validateHasChoices(response);
 
-    if (hasToolCalls(response)) {
+    if (hasToolCalls(response))
       dispatcher(response.choices[0].message.tool_calls![0]);
-    }
+    else
+      console.log(response.choices[0].message.content);
 
   } catch (error) {
 
