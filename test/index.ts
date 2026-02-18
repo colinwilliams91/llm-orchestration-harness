a60073be2f82e59851834e4f1e44fa912bdebce7
colin <colin.williams.dev@gmail.com>
2026-02-19 03:38:24 +0700

    :construction: test sandbox/REPL file working

---
+++ test/index.ts
@@ -0,0 +1,12 @@
+import { processResponseMessage } from "../app/utils/index";
+
+/**
+ * This is like a REPL to test out any of the utility functions
+ * @note run with `bun .\test\index.ts`
+ */
+
+processResponseMessage(`This is a test response message from the utils test file. It has extra spaces and newlines that should be cleaned up.
+
+Let's see if it works! (no special characters)`);
+
+processResponseMessage("\n hi \n this \n has \n new lines.");
