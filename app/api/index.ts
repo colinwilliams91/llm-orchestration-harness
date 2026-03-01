7279e3acc0f9f29f914dabbb9a22b820b01860e3
colin <colin.williams.dev@gmail.com>
2026-03-01 09:48:54 +0700

    codecrafters submit [skip ci]

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,10 +1,11 @@
 import fs from "node:fs";
-import { execSync } from "node:child_process";
+import { exec, execSync } from "node:child_process";
 import type { ChatCompletionMessageToolCall } from "openai/resources";
 import { ERRORS, TOOL_NAMES, type BashArgs, type ReadFileArgs, type WriteFileArgs } from "../constants";
 import { isValidToolName, processResponseMessage, validateArgsHaveFilePath, validateFunctionTool } from "../utils";
 
 // TODO: add BPE (tokenizer) service layer that all dispatched tool calls go through (caching layer in main())
+// TODO: explore more optimizations and sanitization in that service layer or in the dispatched functions
 
 const _read = (args: string): string => {
     const parsed = JSON.parse(args) satisfies ReadFileArgs;
@@ -34,8 +35,6 @@ const _write = (args: string): string => {
 };
 
 const _bash = (args: string): string => {
-    console.log("inside bash tool !!");
-    console.log(args);
     const parsed = JSON.parse(args) satisfies BashArgs;
     return execSync(parsed.command, { encoding: "utf-8" });
 };
